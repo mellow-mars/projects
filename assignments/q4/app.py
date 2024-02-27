679c91a61bcac377c1449b7b3ff5c5f168ea12f2
BeomJung <kbeom0124@naver.com>
2024-02-27 15:52:43 +0900

    added commit to database

--- assignments/q4/app.py
+++ assignments/q4/app.py
@@ -1,11 +1,26 @@
 import random
 from flask import Flask, render_template, request
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'database.db')
+db = SQLAlchemy(app)
 
 hands = ('가위', '바위', '보')
 win = 0
 lose = 0
 draw = 0
+
+
+class GameHistory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_choice = db.Column(db.String, nullable=False)
+    computer_choice = db.Column(db.String, nullable=False)
+    result = db.Column(db.String, nullable=False)
 
 
 def rock_paper_scissor():
@@ -37,7 +52,17 @@ def rock_paper_scissor():
 
 @app.route('/')
 def home():
-    send_result = rock_paper_scissor()
+    user_choice = request.args.get('hands')
+    if user_choice == None:
+        db.session.query(GameHistory).delete()
+        db.session.commit()
+        return render_template('index.html')
+    else:
+        send_result = rock_paper_scissor()
+        game = GameHistory(
+            user_choice=send_result[0], computer_choice=send_result[1], result=send_result[2])
+        db.session.add(game)
+        db.session.commit()
     return render_template('index.html', data=send_result)
 
 
