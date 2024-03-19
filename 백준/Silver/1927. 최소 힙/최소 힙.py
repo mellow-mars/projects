504c40b8dfcacdd4f4dff0eef162c09254211aae
mellow-mars <142032967+mellow-mars@users.noreply.github.com>
2024-03-19 14:53:06 +0900

    [Silver II] Title: 최소 힙, Time: 120 ms, Memory: 37044 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,15 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+n = int(input())
+numbers = []
+for _ in range(n):
+    x = int(input())
+    if x > 0:
+        heapq.heappush(numbers, x)
+    elif x == 0:
+        if numbers:
+            print(heapq.heappop(numbers))
+        else:
+            print(0)
