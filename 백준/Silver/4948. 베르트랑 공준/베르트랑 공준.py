e00fddde40303786a088e1f974704313eb6fc402
mellow-mars <142032967+mellow-mars@users.noreply.github.com>
2024-03-20 13:21:41 +0900

    [Silver II] Title: 베르트랑 공준, Time: 208 ms, Memory: 34604 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -0,0 +1,14 @@
+m = 123456*2
+primes = list(1 for i in range(m+1))
+primes[1]= 0
+for i in range(2,int(m)+1):
+    if primes[i]:
+        for j in range(2*i, int(m)+1, i):
+            primes[j] = 0
+
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    else:
+        print(sum(primes[n+1:2*n+1]))
