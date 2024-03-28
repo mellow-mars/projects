27e006faff19d63f0a5d7f4b3bd0a8ce0e0fa3de
mellow-mars <142032967+mellow-mars@users.noreply.github.com>
2024-03-28 09:36:54 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.py
@@ -0,0 +1,10 @@
+def solution(s):
+    temp = list(s)
+    if temp[0].isnumeric():
+        return int(''.join(temp))
+    else:
+        if temp[0] == '-':
+            return int(''.join(temp[1:]))*-1
+        else:
+            return int(''.join(temp[1:]))
+        
