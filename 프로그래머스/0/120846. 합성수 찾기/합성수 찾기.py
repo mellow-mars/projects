cbf742537c22a16d358692eab5d6dd5238236869
mellow-mars <142032967+mellow-mars@users.noreply.github.com>
2024-04-02 10:08:21 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -0,0 +1,17 @@
+def check(n):
+    count = 0
+
+    for i in range(1,n+1):
+        if n%i == 0:
+            count += 1
+
+    return count
+
+def solution(k):
+    answer = 0
+
+    for n in range(1,k+1):
+        if check(n) > 2 :
+            answer += 1
+
+    return answer
